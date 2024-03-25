ae59687bfc04a2c69616591ea0a70585a3823957
nikola1893 <mitev.nkl@gmail.com>
2024-03-25 15:16:46 +0100

    confirmable devise, sent email after order confirmed

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,8 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :stops, dependent: :destroy
   accepts_nested_attributes_for :stops, allow_destroy: true
-  after_create :send_confirmation_email, :generate_token, :generate_number
+  after_create :generate_token, :generate_number, :send_confirmation_email_to_visitor
+  after_update :send_confirmation_email_to_user, if: :status_changed_to_confirmed?
 
   def load_count
     stops.where(kind: 'load').count
@@ -14,6 +15,10 @@ class Order < ApplicationRecord
   end
 
   private
+
+  def status_changed_to_confirmed?
+    saved_change_to_status? && status == 'confirmed'
+  end
 
   def generate_token
     token = SecureRandom.hex(10)
@@ -26,15 +31,28 @@ class Order < ApplicationRecord
     update(number: "#{datetime}-#{prefix.upcase}")
   end
 
-  def send_confirmation_email
+  def send_confirmation_email_to_visitor
     client = Postmark::ApiClient.new(ENV['POSTMARK_API_TOKEN'])
     confirmation_link = "#{Rails.application.config.domain_name}/orders/#{confirmation_token}/view"
-
+    
     client.deliver(
       from: 'info@deezpatch.com',
       to: self.email,
       subject: 'You have a new dispatch.',
       html_body: "<strong>Hello</strong> please confirm the transport order at this <a href='#{confirmation_link}'>link</a>",
+      track_opens: true,
+      message_stream: 'outbound')
+  end
+
+  def send_confirmation_email_to_user
+    client = Postmark::ApiClient.new(ENV['POSTMARK_API_TOKEN'])
+    confirmation_link = "#{Rails.application.config.domain_name}/orders/#{confirmation_token}/view"
+    
+    client.deliver(
+      from: 'info@deezpatch.com',
+      to: self.user.email,
+      subject: 'Your dispatch has been confirmed.',
+      html_body: "<strong>Hello</strong> your dispatch has been confirmed, view your confirmation on this <a href='#{confirmation_link}'>link</a>",
       track_opens: true,
       message_stream: 'outbound')
   end
